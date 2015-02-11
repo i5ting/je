2d30ce763c990f2d28b71c95590cfdc9c95fcc94
alfred sang <i5ting@126.com>
2015-02-11 22:15:54 +0800

    doc

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.static(path.join(__dirname, 'vendor/3.3.0_0/')));
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
+// 随机端口3000 - 10000 之间
 app.listen(3000)
 
-open("http://127.0.0.1:3000");
+open("http://127.0.0.1:3024");
 
