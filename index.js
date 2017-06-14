5a5584cab2fa9d27521e8e5afdbf4aa38260ef37
shilong.sang <i5ting@126.com>
2017-06-14 11:54:52 +0800

    refact, please ignore

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 
-var express  = require('express');
-var app      = express();
-var path     = require('path');
-var open     = require("open");
+var express  = require('express')
+var path     = require('path')
+var open     = require("open")
 
-app.use(express.static(path.join(__dirname, 'vendor/3.3.0_0/')));
+var app      = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-// 随机端口3000 - 10000 之间
+// 指定静态资源目录
+app.use(express.static(path.join(__dirname, 'vendor/3.3.0_0/')))
+
+// 启动服务器
 app.listen(3024)
 
-open("http://127.0.0.1:3024");
-
+open("http://127.0.0.1:3024")
